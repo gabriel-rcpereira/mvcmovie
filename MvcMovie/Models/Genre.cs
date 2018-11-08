1852142daebd19a752ed569d4ad8b5e34a791261
Gabriel Pereira <gabrielrcpereira@gmail.com>
2018-11-08 21:24:13 -0200

    adding genre entity

---
+++ MvcMovie/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcMovie.Models
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies;
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
